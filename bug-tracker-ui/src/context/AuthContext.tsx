27406221d38d5378fbf01a5fa703ec1d33844b6f
Shehab Shalan <shehab.shalan@hotmail.com>
2022-08-15 22:51:59 +0200

    logout and signup

--- bug-tracker-ui/src/context/AuthContext.tsx
+++ bug-tracker-ui/src/context/AuthContext.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createContext, useContext, useState } from "react";
 import axios from "axios";
 import { Endpoints } from "../services/endpoints";
 import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+
 const AuthContext = createContext<any>({} as any);
 
 export const AuthContextProvider = ({
@@ -10,7 +12,8 @@ export const AuthContextProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const [user, setUser] = useState(localStorage.getItem("token") || null);
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  const [user, setUser] = useState();
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const login = async (email: string, password: string) => {
@@ -31,18 +34,63 @@ export const AuthContextProvider = ({
     }
   };
   const logout = () => {
-    localStorage.removeItem("user");
     localStorage.removeItem("token");
-    setUser(null);
+    setToken(null);
     navigate("/login");
   };
+
+  const signup = async (
+    name: string,
+    email: string,
+    password: string,
+    passwordConfirmation: string
+  ) => {
+    try {
+      setLoading(true);
+      const res = await axios.post(Endpoints.adminSignUp, {
+        email,
+        password,
+        passwordConfirmation,
+        name,
+        role: "admin",
+      });
+
+      window.location.href = "/login";
+    } catch (error: any) {
+      setLoading(false);
+      alert(error.response.data);
+    }
+  };
+  // const verify = async () => {
+  //   try {
+  //     const token = localStorage.getItem("token");
+  //     if (token) {
+  //       const decoded = jwt_decode(token);
+  //       const res = await axios.get(Endpoints.verifySession, {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       });
+  //       if (res.data.user) {
+  //         setUser(res.data.user);
+  //       }
+  //     }
+  //   } catch (error: any) {
+  //     alert(error.response.data);
+  //   }
+  // };
+  // useEffect(() => {
+  //   verify();
+  // }, [token]);
 
   return (
     <AuthContext.Provider
       value={{
         user,
+        token,
         login,
         logout,
+        signup,
         loading,
       }}
     >
