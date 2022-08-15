67afd8fed8b36d49924ae879d0f66ad0cc47b3fc
Shehab Shalan <shehab.shalan@hotmail.com>
2022-08-15 03:00:36 +0200

    account

--- bug-tracker-api/src/controller/account.controller.ts
+++ bug-tracker-api/src/controller/account.controller.ts
@@ -5,6 +5,7 @@ export const getAccountStatsHandler = async (req: Request, res: Response) => {
   const user = res.locals.user;
   try {
     const stats = await getAccountStats(user);
+
     return res.status(200).json({
       status: "success",
       result: stats,
