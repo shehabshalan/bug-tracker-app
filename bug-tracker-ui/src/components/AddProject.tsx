93cb26292357ed8a55e05d662c432b0127f53152
Shehab Shalan <shehab.shalan@hotmail.com>
2022-08-16 05:22:52 +0200

    add project formik

--- bug-tracker-ui/src/components/AddProject.tsx
+++ bug-tracker-ui/src/components/AddProject.tsx
@@ -8,53 +8,71 @@ import {
   TextField,
 } from "@mui/material";
 import { useAppContext } from "../context/AppContext";
-import ContentMultiSelect from "./ContentMultiSelect";
-import ContentSelect from "./ContentSelect";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
+const validationSchema = yup.object({
+  title: yup.string().required("title is required"),
+  description: yup.string().required("Password is required"),
+});
 function AddProject() {
-  const memberLabel = "Member";
-  const memberMenuItems = ["Shehab", "Ali", "Ahmed", "Hassan"];
-  const typeLable = "Type";
-  const typeMenuItems = ["Bug", "Feature", "Task"];
-  const priorityLabel = "Priority";
-  const priorityMenuItems = ["Low", "Medium", "High"];
-  const statusLabel = "Status";
-  const statusMenuItems = ["Open", "In Progress", "Closed"];
   const { handleClose, openType } = useAppContext();
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <Dialog open={openType.openProject} onClose={handleClose}>
       <DialogTitle>Create project</DialogTitle>
-      <DialogContent>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sx={{ mt: 1 }}>
-            <TextField
-              autoFocus
-              id="title"
-              label="Title"
-              fullWidth
-              variant="outlined"
-            />
+      <form onSubmit={formik.handleSubmit}>
+        <DialogContent>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sx={{ mt: 1 }}>
+              <TextField
+                fullWidth
+                autoFocus
+                id="title"
+                name="title"
+                label="Title"
+                value={formik.values.title}
+                onChange={formik.handleChange}
+                error={formik.touched.title && Boolean(formik.errors.title)}
+                helperText={formik.touched.title && formik.errors.title}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                autoFocus
+                id="description"
+                name="description"
+                label="Description"
+                multiline
+                rows={4}
+                value={formik.values.description}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.description &&
+                  Boolean(formik.errors.description)
+                }
+                helperText={
+                  formik.touched.description && formik.errors.description
+                }
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              autoFocus
-              id="title"
-              label="Description"
-              fullWidth
-              variant="outlined"
-              rows={4}
-              multiline
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <ContentMultiSelect />
-          </Grid>
-        </Grid>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={handleClose}>Submit</Button>
-      </DialogActions>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Submit</Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
