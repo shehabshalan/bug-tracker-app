d4c3cb6ee13482718d0fee9026d65c5396251a21
Shehab Shalan <shehab.shalan@hotmail.com>
2022-08-18 00:34:59 +0200

    enums added

---
+++ bug-tracker-ui/src/types/enums.ts
@@ -0,0 +1,21 @@
+// const TICKET_TYPE = ["Bug", "Feature", "Task"];
+// const TICKET_PRIORITY = ["Low", "Medium", "High"];
+// const TICKET_STATUS = ["Open", "In Progress", "Closed"];
+
+export enum TicketStatus {
+  Open = "Open",
+  InProgress = "In Progress",
+  Closed = "Closed",
+}
+
+export enum TicketType {
+  Bug = "Bug",
+  Feature = "Feature",
+  Task = "Task",
+}
+
+export enum TicketPriority {
+  Low = "Low",
+  Medium = "Medium",
+  High = "High",
+}
