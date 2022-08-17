283b6aeff6fb159f3a9abb5f2889f91625b61e61
Shehab Shalan <shehab.shalan@hotmail.com>
2022-08-17 03:09:50 +0200

    query provider added

--- bug-tracker-ui/src/index.tsx
+++ bug-tracker-ui/src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
 import { AppContextProvider } from "./context/AppContext";
 import { AuthContextProvider } from "./context/AuthContext";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,7 +17,9 @@ root.render(
     <Router>
       <AuthContextProvider>
         <AppContextProvider>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </AppContextProvider>
       </AuthContextProvider>
     </Router>
