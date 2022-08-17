3d1cda74b2d3e053a059849a5824b55f1b9a1b76
Shehab Shalan <shehab.shalan@hotmail.com>
2022-08-17 03:10:34 +0200

    cleaned up

--- bug-tracker-ui/src/pages/ProjectTickets/index.tsx
+++ bug-tracker-ui/src/pages/ProjectTickets/index.tsx
@@ -5,47 +5,40 @@ import ContentTable from "../../components/ContentTable";
 import Avatar from "@mui/material/Avatar";
 import AvatarGroup from "@mui/material/AvatarGroup";
 import AddIcon from "@mui/icons-material/Add";
-import { Column } from "@material-table/core";
 import ContentDivider from "../../components/ContentDivider";
 import Content from "../../components/Content";
 import AddTicket from "./AddTicket";
 import { useAppContext } from "../../context/AppContext";
 import AddMember from "./AddMember";
 import AlertMessage from "../../components/AlertMessage";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { ticketColumns } from "../../data/ticketColumns";
+import { useParams } from "react-router-dom";
+import { getProjectTickets } from "../../services/api";
 
 function ProjectDetails() {
   const { handleClickOpen } = useAppContext();
+  const { id } = useParams();
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  interface IPerson {
-    firstName: string;
-    lastName: string;
-    birthYear: number;
-    availability: boolean;
-  }
-
-  const lookup = { true: "Available", false: "Unavailable" };
-
-  const columns: Array<Column<IPerson>> = [
-    { title: "First Name", field: "firstName" },
-    { title: "Last Name", field: "lastName" },
-    { title: "Birth Year", field: "birthYear", type: "numeric" },
-    { title: "Availablity", field: "availability", lookup },
-  ];
-
-  const data: Array<IPerson> = [
+  const {
+    data,
+    isLoading,
+    error,
+  }: { data: any; isLoading: any; error: any; status: any } = useQuery(
+    ["project-tickets", id, page],
+    getProjectTickets,
     {
-      firstName: "Tod",
-      lastName: "Miles",
-      birthYear: 1987,
-      availability: true,
-    },
-    {
-      firstName: "Jess",
-      lastName: "Smith",
-      birthYear: 2000,
-      availability: false,
-    },
-  ];
+      keepPreviousData: true,
+      onSuccess: (data) => {
+        setTotalPages(data.totalPages);
+        setLoading(false);
+      },
+    }
+  );
 
   return (
     <ContentPage>
@@ -74,7 +67,15 @@ function ProjectDetails() {
         buttonText={"New Ticket"}
         buttonAction={"openTicket"}
       >
-        {/* <ContentTable data={data} columns={columns} pageSize={data.length} /> */}
+        <ContentTable
+          data={data}
+          columns={ticketColumns}
+          error={error}
+          isLoading={isLoading}
+          setPage={setPage}
+          totalPages={totalPages}
+          page={page}
+        />
       </ContentTab>
       <ContentDivider />
       <ContentTab title={"Ticket Details"}>
