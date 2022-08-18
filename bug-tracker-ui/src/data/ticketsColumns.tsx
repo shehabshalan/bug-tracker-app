05a6e27d98dfacbf9f11bb1324e8015223ae416a
Shehab Shalan <shehab.shalan@hotmail.com>
2022-08-18 02:57:39 +0200

    tickets columns added

---
+++ bug-tracker-ui/src/data/ticketsColumns.tsx
@@ -0,0 +1,27 @@
+import { Column } from "@material-table/core";
+import { Link } from "react-router-dom";
+import { ITicket } from "../types/ITicket";
+import dateConverter from "../utils/dateConverter";
+
+export const projectColumns: Array<Column<ITicket>> = [
+  {
+    title: "Ticket Name",
+    field: "ticketName",
+    render: (rowData) => (
+      <Link to={`/project/${rowData._id}`} style={{ color: "black" }}>
+        {rowData.ticketName}
+      </Link>
+    ),
+  },
+  { title: "Project", field: "ticketProject.projectName" },
+  { title: "Type", field: "ticketType" },
+  { title: "Status", field: "ticketStatus" },
+  { title: "Priority", field: "ticketPriority" },
+  {
+    title: "Created",
+    field: "createdAt",
+    render: (rowData: ITicket) => {
+      return dateConverter(rowData.createdAt);
+    },
+  },
+];
