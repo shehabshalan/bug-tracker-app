49b4601364ed4f173a0ca6031663e36c6f183025
Shehab Shalan <shehab.shalan@hotmail.com>
2022-08-18 02:58:25 +0200

    ticket details page

--- bug-tracker-ui/src/pages/TicketDetails/index.tsx
+++ bug-tracker-ui/src/pages/TicketDetails/index.tsx
@@ -1,7 +1,62 @@
 import React from "react";
+import Ticket from "./Ticket";
+import ContentTab from "../../components/ContentTab";
+import { getProjectMembers, getTicketById } from "../../services/api";
+import { useParams } from "react-router-dom";
+import { useAppContext } from "../../context/AppContext";
+import { useQuery } from "@tanstack/react-query";
+import EditTicket from "./EditTicket";
 
 function TicketDetails() {
-  return <div>TicketDetails</div>;
+  const { setError, setMessage } = useAppContext();
+  const { id } = useParams();
+
+  const { data, isLoading, error } = useQuery(
+    ["ticket-details", id],
+    getTicketById,
+    {
+      keepPreviousData: true,
+      onError: () => {
+        setError(true);
+        setMessage("Error fetching tickets");
+      },
+    }
+  );
+
+  const { data: projectDetails }: { data: any; isLoading: any; error: any } =
+    useQuery(
+      ["project-details2", data?.ticketProject?._id],
+      getProjectMembers,
+      {
+        keepPreviousData: true,
+        onError: () => {
+          setError(true);
+          setMessage("Error fetching members");
+        },
+      }
+    );
+  if (error) {
+    return <div>Error</div>;
+  }
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+  return (
+    <div>
+      <ContentTab
+        title={"Ticket Details"}
+        buttonText={"Update Ticket"}
+        buttonAction={"openEditTicket"}
+      >
+        <Ticket ticket={data} />
+      </ContentTab>
+      <EditTicket
+        projectId={data?.ticketProject?._id}
+        membersData={projectDetails}
+        ticket={data}
+      />
+    </div>
+  );
 }
 
 export default TicketDetails;
