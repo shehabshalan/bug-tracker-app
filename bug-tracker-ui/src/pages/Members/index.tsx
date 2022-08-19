90313d9dca77d7aba3db297a3c62e4aef38e237d
shehabshalan <shehab.shalan@hotmail.com>
2022-08-19 03:37:24 +0200

    all members page

--- bug-tracker-ui/src/pages/Members/index.tsx
+++ bug-tracker-ui/src/pages/Members/index.tsx
@@ -1,19 +1,34 @@
 import React from "react";
 import AddNewMember from "./AddNewMember";
 import ContentTab from "../../components/ContentTab";
+import ContentPage from "../../components/ContentPage";
+import ContentTable from "../../components/ContentTable";
+import { Typography } from "@mui/material";
 
 function Members() {
   return (
-    <div>
+    <ContentPage>
+      <Typography variant="h6" gutterBottom>
+        All Members
+      </Typography>
+
       <ContentTab
         title={"Members"}
         buttonText={"New Member"}
         buttonAction={"openUser"}
       >
-        <div>Members</div>
+        {/* <ContentTable
+          data={data}
+          columns={projectColumns}
+          error={error}
+          isLoading={isLoading}
+          setPage={setPage}
+          totalPages={totalPages}
+          page={page}
+        /> */}
       </ContentTab>
       <AddNewMember />
-    </div>
+    </ContentPage>
   );
 }
 
