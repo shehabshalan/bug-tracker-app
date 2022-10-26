ee8cde1a601599638eecc4d865ae046067305473
shehabshalan <shehab.shalan@hotmail.com>
2022-10-27 01:02:01 +0200

    updated api url

--- bug-tracker-ui/src/api/endpoints.ts
+++ bug-tracker-ui/src/api/endpoints.ts
@@ -2,7 +2,7 @@ export class Endpoints {
   public static baseUrl =
     process.env.NODE_ENV === "development"
       ? "http://localhost:1337/api/"
-      : "https://reactblog-strapi.herokuapp.com/api/";
+      : "https://limitless-reef-02128.herokuapp.com/api/";
   public static login = Endpoints.baseUrl + "sessions";
   public static adminSignUp = Endpoints.baseUrl + "admin";
   public static createMember = Endpoints.baseUrl + "members";
