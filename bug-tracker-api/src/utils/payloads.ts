0dde3d38c0ffe3818e2f461000675b62109cc55c
shehabshalan <shehab.shalan@hotmail.com>
2022-11-02 17:33:14 +0100

    user payload for testing

---
+++ bug-tracker-api/src/utils/payloads.ts
@@ -0,0 +1,7 @@
+export const userPayload = {
+  _id: "62fda2c6288fbced0166f5a1",
+  name: "demo",
+  email: "demo@demo.com",
+  role: "admin",
+  slug: "BT-1a29a8d1-7b76-42d3-afa0-c86706d179e1",
+};
