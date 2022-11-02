660415e0fecc93cdcd3dc7b8b4a7867ee6e7b836
shehabshalan <shehab.shalan@hotmail.com>
2022-11-02 17:50:43 +0100

    testing post request

--- bug-tracker-api/src/__tests__/project.test.ts
+++ bug-tracker-api/src/__tests__/project.test.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import supertest from "supertest";
 import createServer from "../utils/createServer";
 import { signJwt } from "../utils/jwt";
-import { userPayload } from "../utils/payloads";
+import { userPayload, projectPayload } from "../utils/payloads";
 import config from "config";
 
 const projectId = "6359baed64ae90e8680b48ae";
@@ -18,8 +18,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe("Project", () => {
-  describe("Get Project", () => {
+describe("Projects", () => {
+  describe("Get Projects --> /api/projects", () => {
+    describe("given a user not logged in", () => {
+      it("should return 401", async () => {
+        const res = await supertest(app).get(`/api/projects`);
+        expect(res.status).toBe(401);
+      });
+    });
+    describe("given a user is logged in", () => {
+      it("should return 200", async () => {
+        const token = signJwt(userPayload);
+        const res = await supertest(app)
+          .get(`/api/projects`)
+          .set("Authorization", `Bearer ${token}`);
+        expect(res.status).toBe(200);
+      });
+    });
+  });
+  describe("Get Project By Id --> /api/projects/:id", () => {
     describe("given a user not logged in", () => {
       it("should return 401", async () => {
         const res = await supertest(app).get(`/api/projects/${projectId}`);
@@ -51,6 +68,35 @@ describe("Project", () => {
           .get(`/api/projects/${projectId}`)
           .set("Authorization", `Bearer ${token}`);
         expect(res.status).toBe(200);
+      });
+    });
+  });
+
+  describe("Create Project --> /api/projects", () => {
+    describe("given a user not logged in", () => {
+      it("should return 401", async () => {
+        const res = await supertest(app).post(`/api/projects`);
+        expect(res.status).toBe(401);
+      });
+    });
+    describe("given a user is logged in but project payload is missing", () => {
+      it("should return 400", async () => {
+        const token = signJwt(userPayload);
+        const res = await supertest(app)
+          .post(`/api/projects`)
+          .set("Authorization", `Bearer ${token}`);
+        expect(res.status).toBe(400);
+      });
+    });
+
+    describe("given a user is logged in", () => {
+      it("should create project and return 201", async () => {
+        const token = signJwt(userPayload);
+        const res = await supertest(app)
+          .post(`/api/projects`)
+          .set("Authorization", `Bearer ${token}`)
+          .send(projectPayload);
+        expect(res.status).toBe(201);
       });
     });
   });
