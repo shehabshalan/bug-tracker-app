cf27d8c1cf9ccff53f6750603a26f574a6b06135
shehabshalan <shehab.shalan@hotmail.com>
2022-11-02 17:32:26 +0100

    moved app for testing

--- bug-tracker-api/src/app.ts
+++ bug-tracker-api/src/app.ts
@@ -2,21 +2,15 @@ import express from "express";
 import config from "config";
 import dbConnect from "./utils/dbConnect";
 import logger from "./utils/logger";
-import cors, { CorsOptions } from "cors";
-import routes from "./routes";
-import corsOptions from "./utils/corsOptions";
+import createServer from "./utils/createServer";
 
 require("dotenv").config();
 
 const PORT = config.get<number>("port");
-export const app = express();
 
-app.use(cors(corsOptions as CorsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = createServer();
 
 app.listen(PORT, async () => {
   logger.info(`Api is running at port ${PORT}`);
   await dbConnect();
-  routes(app);
 });
